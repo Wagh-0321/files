48cf165b502f664ab6afe9126bfae117a5040a17
Wagh-0321 <43107905+Wagh-0321@users.noreply.github.com>
2018-11-27 21:02:40 +0530

    Update myjs-1.js

--- myjs-1.js
+++ myjs-1.js
@@ -3,18 +3,20 @@
    //btn1
     var btn=document.getElementById("btn1");
     
-    btn.addEventListener("click",function(){
-                 //AJAX
-    var ourRequest=new XMLHttpRequest();
-    ourRequest.open('GET','https://wagh-0321.github.io/files/myapp-1.json');
-    ourRequest.onload=function(){
-    var ourData=JSON.parse(ourRequest.responseText);
-      //here
-      renderHTML(ourData);
-    };
-    ourRequest.send();
+    btn.addEventListener("click",alert("hello")
+//                          function(){
+//                  //AJAX
+//     var ourRequest=new XMLHttpRequest();
+//     ourRequest.open('GET','https://wagh-0321.github.io/files/myapp-1.json');
+//     ourRequest.onload=function(){
+//     var ourData=JSON.parse(ourRequest.responseText);
+//       //here
+//       renderHTML(ourData);
+//     };
+//     ourRequest.send();
       
-                 });
+//                  }
+                        );
     function renderHTML(data){
       
      div1.insertAdjacentHTML('beforeend','ok');    }
